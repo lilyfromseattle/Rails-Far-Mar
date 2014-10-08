6f5e92f2bec5843b0a55044c8754f8e8d137e8bc
Lily Pace <lilyfpace@gmail.com>
2014-10-08 16:37:14 -0700

    product stuff

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    def index
+  def index
     @products = Product.all
   end
 
@@ -9,14 +9,20 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(params.require(:product).permit(:name))
+    # raise params.inspect
     if @product.save
-      redirect_to root_path
+      redirect_to "/products"
     else
+      redirect_to "/products/new"    
     end
   end
 
   def destroy
-    self.delete
+    # raise params.inspect
+    @product = Product.all.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
+    # @product.destroy
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,13 @@ Rails.application.routes.draw do
 
    get 'home/index'
 
+
   get  "/products",     to: "products#index"
-  post "/products",     to: "products#create"
+  post "/products/new", to: "products#create"
   get  "/products/new", to: "products#new"
+  get "/products/destroy/:id",  to: "products#destroy"
+
+  root "products#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
