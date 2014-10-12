1754a18a4069ecb772c5e889399e1a03526cf46d
Calla Patel <callapatel@gmail.com>
2014-10-11 17:41:10 -0700

    added config.serve_static_assets = true into the config file so it renders in heroku

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
+config.serve_static_assets = true
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
