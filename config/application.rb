8fa3c06248ff62ae63c81ebeebac9fcfaf46d84a
Calla Patel <callapatel@gmail.com>
2014-10-11 17:51:44 -0700

    bundle updates

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,9 @@
+
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
 config.serve_static_assets = true
-gem 'rails_12factor', group: :production
+
 require "active_model/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
